2e3bd300d9b4a40c28b55bd636eaa0545c298f08
Nguyen Tuyet <125747869+DRM-T@users.noreply.github.com>
2023-12-20 18:08:12 +0700

    Update AbstractComposite.java

--- Bai1/AbstractComposite.java
+++ Bai1/AbstractComposite.java
@@ -3,23 +3,26 @@ package DesignPatern.Bai1;
 import java.util.Collections;
 import java.util.List;
 
-/**yaya.*/
 public abstract class AbstractComposite implements Composite {
+    /**yaya.*/
     @Override
     public boolean isGroup() {
         return false;
     }
 
+    /**yaya.*/
     @Override
     public boolean isMarried() {
         return false;
     }
 
+    /**yaya.*/
     @Override
     public void addChild(Composite child) {
         throw new UnsupportedOperationException("Operation not supported for this type");
     }
 
+    /**yaya.*/
     @Override
     public List<Person> getChildren() {
         return Collections.emptyList();
