eb42f77eab6c7ba1e9c863c03096632e3a5ee76f
Nguyen Tuyet <125747869+DRM-T@users.noreply.github.com>
2023-12-20 18:08:33 +0700

    Update Couple.java

--- Bai1/Couple.java
+++ Bai1/Couple.java
@@ -4,19 +4,23 @@ public class Couple {
     private final Person husband;
     private final Person wife;
 
+    /**yaya.*/
     public Couple(Person husband, Person wife) {
         this.husband = husband;
         this.wife = wife;
     }
 
+    /**yaya.*/
     public Person getHusband() {
         return husband;
     }
 
+    /**yaya.*/
     public Person getWife() {
         return wife;
     }
 
+    /**yaya.*/
     @Override
     public String toString() {
         return String.format("%s - %s", husband.getName(), wife.getName());
