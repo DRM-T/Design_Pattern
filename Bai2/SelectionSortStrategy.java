7ec8d3eff3642bfd7546969f059dc27575e8109f
Nguyen Tuyet <125747869+DRM-T@users.noreply.github.com>
2023-12-20 16:52:25 +0700

    Add files via upload

---
+++ Bai2/SelectionSortStrategy.java
@@ -0,0 +1,30 @@
+package DesignPatern.Bai2;
+
+public class SelectionSortStrategy extends Strategy {
+
+    @Override
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = findMinIndex(array, i);
+            if (minIndex != i) {
+                swap(array, i, minIndex);
+            }
+        }
+    }
+
+    private int findMinIndex(int[] array, int start) {
+        int minIndex = start;
+        for (int j = start + 1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        return minIndex;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
