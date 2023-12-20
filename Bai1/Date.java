348b9759f12c3a5518362b7b9e829dda06fa194c
Nguyen Tuyet <125747869+DRM-T@users.noreply.github.com>
2023-12-20 18:08:56 +0700

    Update Date.java

--- Bai1/Date.java
+++ Bai1/Date.java
@@ -5,36 +5,44 @@ public class Date {
     private int month;
     private int day;
 
+    /**yaya.*/
     public Date(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
+    /**yaya.*/
     public int getYear() {
         return year;
     }
 
+    /**yaya.*/
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**yaya.*/
     public int getMonth() {
         return month;
     }
 
+    /**yaya.*/
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**yaya.*/
     public int getDay() {
         return day;
     }
 
+    /**yaya.*/
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**yaya.*/
     @Override
     public String toString() {
         return String.format("%04d-%02d-%02d", year, month, day);
